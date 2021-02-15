94a60b1832b9322691bc646928324ff04305c815
Arthur Soulié <arthur.soulie@tuta.io>
2021-02-15 21:43:37 +0100

    Fix realloc's memcpy & update structure

--- src/realloc.c
+++ src/realloc.c
@@ -9,18 +9,27 @@
 #include <unistd.h>
 #include <string.h>
 
+static void *allocate_of_size(void *ptr, size_t size)
+{
+    void *new_ptr = malloc(size);
+    meta_t *meta = (meta_t *)(ptr - sizeof(meta_t));
+
+    if (!new_ptr)
+        return (NULL);
+    memcpy(new_ptr, ptr, (ptr + meta->size) - ptr);
+    free(ptr);
+    return (new_ptr);
+}
+
 void *realloc(void *ptr, size_t size)
 {
-    void *new = NULL;
-    meta_t *meta = NULL;
-
-    if (!ptr)
+    if (!ptr && size)
         return malloc(size);
-    meta = (meta_t *)(ptr - sizeof(meta_t));
-    new = malloc(size);
-    if (!new)
+    if (ptr && !size) {
+        free(ptr);
         return (NULL);
-    memcpy(new, ptr, ptr - (ptr + meta->size));
-    free(ptr);
-    return (new);
+    }
+    if (!ptr && !size)
+        return (NULL);
+    return (allocate_of_size(ptr, size));
 }
