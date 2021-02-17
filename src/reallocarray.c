206db96fa1bff30b763f11fea9bce8a93924b393
Arthur Soulié <arthur.soulie@tuta.io>
2021-02-17 10:55:53 +0100

    Add reallocarray

--- src/reallocarray.c
+++ src/reallocarray.c
@@ -6,8 +6,16 @@
 */
 
 #include <stddef.h>
+#include "malloc.h"
 
 void *reallocarray(void *ptr, size_t nmemb,size_t size)
 {
+    size_t alloc_size = nmemb * size;
+
+    if (!nmemb || !size || ptr)
+        return (NULL);
+    if (nmemb && alloc_size / nmemb != size)
+        return (NULL);
+    realloc(ptr, alloc_size);
     return (NULL);
 }
