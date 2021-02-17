072ea54086258477875556a64dd33391237b621a
Arthur Soulié <arthur.soulie@tuta.io>
2021-02-17 10:55:44 +0100

    Add calloc

--- src/calloc.c
+++ src/calloc.c
@@ -5,10 +5,23 @@
 ** calloc function
 */
 
+#include <string.h>
+#include <limits.h>
 #include <stddef.h>
 #include "malloc.h"
 
 void *calloc(size_t nmemb, size_t size)
 {
-    return (NULL);
+    void *allocated = NULL;
+    size_t alloc_size = nmemb * size;
+
+    if (!nmemb || !size)
+        return (NULL);
+    if (nmemb && alloc_size / nmemb != size)
+        return (NULL);
+    allocated = malloc(alloc_size);
+    if (!allocated)
+        return (NULL);
+    memset(allocated, 0, alloc_size);
+    return (allocated);
 }
